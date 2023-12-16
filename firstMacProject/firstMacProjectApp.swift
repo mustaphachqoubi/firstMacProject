ed4e2bb91c86842bf99134dc5d6a52c0fd15859c
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-12-16 16:14:24 +0100

    Initial Commit

---
+++ firstMacProject/firstMacProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstMacProjectApp.swift
+//  firstMacProject
+//
+//  Created by mustapha chqoubi on 16/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct firstMacProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

842864eb79e244b5f6b92b86fe5d003d78a5b8d1
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-12-17 15:46:08 +0100

    i am just testing the git with the xcode status

--- firstMacProject/ContentView.swift
+++ firstMacProject/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "person")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("mustapha chqoubi")
         }
         .padding()
     }

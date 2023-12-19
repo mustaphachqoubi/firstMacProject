586c7d42d2a31bed82c410b64276130d809aa3a7
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-12-19 20:36:46 +0100

    variables and types

--- firstMacProject/ContentView.swift
+++ firstMacProject/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
     
     // i am learning swift right now
     // i learned commands, for loops condational code, logical operators
+    // now i passed some variables and types challenges
 }
 
 #Preview {

0be10e916db06afa03f927e5f61432002942bb52
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-12-20 20:41:32 +0100

    i learned initialization and functions, and parameters

--- firstMacProject/ContentView.swift
+++ firstMacProject/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
     // i am learning swift right now
     // i learned commands, for loops condational code, logical operators
     // now i passed some variables and types challenges
+    // initialization and functions, and parameters
 }
 
 #Preview {
